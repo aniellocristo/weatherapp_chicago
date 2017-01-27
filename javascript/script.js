7fcba6a23b2cbee67e3b2b172e0ed30e72e2badb
Aniello Cristo <aniello.cristo@gmail.com>
2017-01-27 13:33:47 -0600

    F to C conversion added, toggle switch removed

--- javascript/script.js
+++ javascript/script.js
@@ -26,6 +26,7 @@ console.log(api);
 $.getJSON(api, function(data){
   //Current Forecast Variables
   var fTemp1 = data.current.temp_f;
+  var cTemp1 = data.current.temp_c;
   var wIcon1 = data.current.condition.code;
   var humid1 = data.current.humidity;
   var wind1 = data.current.wind_mph;
@@ -33,22 +34,30 @@ $.getJSON(api, function(data){
 
   //second day forecast variables
   var fTempMax2 = data.forecast.forecastday[0].day.maxtemp_f;
+  var cTempMax2 = data.forecast.forecastday[0].day.maxtemp_c;
   var fTempMin2 = data.forecast.forecastday[0].day.mintemp_f;
+  var cTempMin2 = data.forecast.forecastday[0].day.mintemp_c;
   var wIcon2 = data.forecast.forecastday[0].day.condition.code;
 
   //third day forecast variables
   var fTempMax3 = data.forecast.forecastday[1].day.maxtemp_f;
+  var cTempMax3 = data.forecast.forecastday[1].day.maxtemp_c;
   var fTempMin3 = data.forecast.forecastday[1].day.mintemp_f;
+  var cTempMin3 = data.forecast.forecastday[1].day.mintemp_c;
   var wIcon3 = data.forecast.forecastday[1].day.condition.code;
 
   //fourth day forecast variables
   var fTempMax4 = data.forecast.forecastday[2].day.maxtemp_f;
+  var cTempMax4 = data.forecast.forecastday[2].day.maxtemp_c;
   var fTempMin4 = data.forecast.forecastday[2].day.mintemp_f;
+  var cTempMin4 = data.forecast.forecastday[2].day.mintemp_c;
   var wIcon4 = data.forecast.forecastday[2].day.condition.code;
 
   //fifith day forecast variables
   var fTempMax5 = data.forecast.forecastday[3].day.maxtemp_f;
+  var cTempMax5 = data.forecast.forecastday[3].day.maxtemp_c;
   var fTempMin5 = data.forecast.forecastday[3].day.mintemp_f;
+  var cTempMin5 = data.forecast.forecastday[3].day.mintemp_c;
   var wIcon5 = data.forecast.forecastday[3].day.condition.code;
 
 //Determine if it is daytime or night time for icon
@@ -61,28 +70,44 @@ if (dayTime === 1) {
 //Assigning information to current weather
   $("#icon-1")[0].setAttribute("src", "images/"+ wIcon1 + dayTime + ".png");
   $("#temp-1").html(Math.round(fTemp1) + " &#8457;");
+  $("#tempc-1").html(Math.round(cTemp1) + " &#8451;");
   $("#humid-1").html(humid1 + "&#37;");
   $("#wind-1").html(Math.round(wind1) + " mph");
 
 //Assigning information to day 1 html elements
   $("#icon-2")[0].setAttribute("src", "images/"+ wIcon2 + ".png");
   $("#temp-max-2").html(Math.round(fTempMax2) + "&#8457;");
+  $("#tempc-max-2").html(Math.round(cTempMax2) + "&#8451;");
   $("#temp-min-2").html(Math.round(fTempMin2) + "&#8457;");
+  $("#tempc-min-2").html(Math.round(cTempMin2) + "&#8451;");
 
 //Assign informtion to day 2 html elements
   $("#icon-3")[0].setAttribute("src", "images/"+ wIcon3 + ".png");
   $("#temp-max-3").html(Math.round(fTempMax3) + "&#8457;");
+  $("#tempc-max-3").html(Math.round(cTempMax3) + "&#8451;");
   $("#temp-min-3").html(Math.round(fTempMin3) + "&#8457;");
+  $("#tempc-min-3").html(Math.round(cTempMin3) + "&#8451;");
 
 //Assign information to day 3 html elemtns
   $("#icon-4")[0].setAttribute("src", "images/"+ wIcon4 + ".png");
   $("#temp-max-4").html(Math.round(fTempMax4) + "&#8457;");
+  $("#tempc-max-4").html(Math.round(cTempMax4) + "&#8451;");
   $("#temp-min-4").html(Math.round(fTempMin4) + "&#8457;");
+  $("#tempc-min-4").html(Math.round(cTempMin4) + "&#8451;");
 
 //Assign information to day 4 html elements
   $("#icon-5")[0].setAttribute("src", "images/"+ wIcon5 + ".png");
   $("#temp-max-5").html(Math.round(fTempMax5) + "&#8457;");
+  $("#tempc-max-5").html(Math.round(cTempMax5) + "&#8451;");
   $("#temp-min-5").html(Math.round(fTempMin5) + "&#8457;");
+  $("#tempc-min-5").html(Math.round(cTempMin5) + "&#8451;");
+
+// Toggle for F to C
+$(".tempbutton").click(function(){
+      $(".tog-f").toggle().delay(400);
+      $(".tog-c").toggle().delay(400);
+});
+
 
 
 //Set background on time of day
